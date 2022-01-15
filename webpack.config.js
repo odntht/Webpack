58c2b91e46bd99e2d599728480b87d25151746ed
Fernando Rodrigues <odntht@gmail.com>
2022-01-15 19:28:55 -0300

    Ajustando Webpack Config pra geração do HTML dinâmico

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './app/src/js/app.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'app/dist')
-    }
-};
+    },
+    plugins: [new HtmlWebPackPlugin()]
+};   
