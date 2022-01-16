fec03e9f926375963f886a5501c8312b5867cfa8
Fernando Rodrigues <odntht@gmail.com>
2022-01-15 21:35:23 -0300

    Add dependecy bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: './app/src/js/app.js',
@@ -8,9 +9,17 @@ module.exports = {
         path: path.resolve(__dirname, 'app/dist'),
         clean: true
     },
-    plugins: [new HtmlWebPackPlugin({
-        template: './app/src/app.html',
-        filename: 'app.html',
-        hash: true
-    })]
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './app/src/app.html',
+            filename: 'app.html',
+            hash: true
+        }),
+        // new CopyWebpackPlugin({
+        //     patterns: [{
+        //         from: './app/src/css',
+        //         to: 'css'
+        //     }]
+        // })
+    ]
 };   
